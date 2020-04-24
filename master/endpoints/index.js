a341cfc3ef661576eb1db08cfac3a321ea4c17cc
YASHYK47 <yashky47@gmail.com>
2020-04-25 03:37:28 +0530

    API Documentation

--- master/endpoints/index.js
+++ master/endpoints/index.js
@@ -3,20 +3,20 @@ const { startTask, stopTask, getTask } = require("../module/task");
 
 router.get("/start", (req, res, next) => {
   startTask()
-    .then((e) => res.json({ msg: "Starting Task..." }))
+    .then((task) => res.json({ msg: "Starting Task..." }))
     .catch((err) => next(new Error(err)));
 });
 
 router.post("/stop", (req, res, next) => {
   stopTask(req.body.id)
-    .then((e) => res.json({ msg: "Stopping Task..." }))
+    .then((task) => res.json({ msg: "Stopping Task..." }))
     .catch((err) => next(new Error(err)));
 });
 
 router.get("/tasks", (req, res, next) => {
   getTask()
-    .then((e) => {
-      res.json({ e });
+    .then((tasks) => {
+      res.json({ tasks });
     })
     .catch((err) => next(err));
 });
